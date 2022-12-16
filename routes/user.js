9b83b7a2eaec59ea7a42419c726db57a4ab65a65
yeashwant <yeashwantbabu@gmail.com>
2022-12-16 16:16:58 +0530

    [feature][delete-user] first commit

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 const router = express.Router();
 
 
-const users = [];
+let users = [];
 
 // GET /users find all users
 router.get('/', ( req, res) => res.send(users));
@@ -22,7 +22,14 @@ router.get('/:id', (req,res)=> {
     const foundUser = users.find((user)=> user.id == id);
 
     res.send(foundUser);
-})
+});
+
+router.delete('/:id', (req,res)=>{
+    const { id } = req.params;
+
+    users = users.filter((user)=> user.id ===! id );
+    res.send(`user with  the id ${id} deleted from the database.`);
+});
 
 
 
